59a92df50f75d8e04641b12d5f40eeda75b8fe99
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-21 12:58:37 -0700

    changed book property to summary

--- server/api/models/bookModel.js
+++ server/api/models/bookModel.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var BookSchema = new mongoose.Schema({
   title: String,
   author: String,
-  summary: String
+  description: String,
+  imageLinks: [{type: String}]
 });
 
 module.exports = mongoose.model('book', BookSchema);
