744bebd6b75cdb0d21bf77b2ce3aaeb18702cf7c
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-21 17:15:05 -0700

    add pagination to view more books

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -2,12 +2,29 @@ angular.module('bookstore.landing', [])
 .controller('LandingController', ['$scope', '$http', 'Landing', function($scope, $http, Landing) {
   $scope.books = [];
   $scope.search = '';
-  $scope.fetchBooks = function(search) {
-    Landing.getBooks(search)
+  $scope.savedSearch = '';
+  $scope.page = 1;
+  $scope.pages = [];
+  $scope.index = 0;
+  $scope.fetchBooks = function(search, index) {
+    $scope.savedSearch = search;
+    $scope.pages = [];
+    Landing.getBooks(search, index)
       .then(function(book) {
         $scope.books = book;
         // clear search field
         $scope.search = '';
+        $scope.pages.push($scope.page);
       });
+  };
+  $scope.nextPage = function(startIndex) {
+    $scope.page++;
+    $scope.pages.push($scope.page);
+    $scope.books = [];
+    $scope.index += 41;
+    Landing.getBooks($scope.savedSearch, $scope.index)
+      .then(function(book) {
+        $scope.books = book;
+      })
   };
 }]);
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,9 +1,9 @@
 angular.module('bookstore.services', [])
 .factory('Landing', function($http) {
-  var getBooks = function(search) {
+  var getBooks = function(search, index) {
     return $http({
       method: 'GET',
-      url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + search + '&printType=books&orderBy=relevance&maxResults=40'
+      url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + search + '&printType=books&orderBy=relevance&filter=ebooks&maxResults=40&startIndex=' + index
     })
     .then(function(res) {
       console.log(res.data.items);
