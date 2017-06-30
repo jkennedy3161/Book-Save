101bc08c121c31989142be4d309f86603467291f
jkennedy3161 <kennedyjt88@gmail.com>
2017-06-30 16:31:29 -0700

    reassign account page for signing in only

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -1,10 +1,22 @@
 angular.module('bookstore.account', [])
-.controller('AccountController', function($scope, $location, Auth) {
-  $scope.login = function(username, password) {
-    var data = {username: username, password: password};
-    Auth.login(data)
-      .then(function() {
-        $location.path('/landing');
+.controller('AccountController', function($scope, $location, Auth, $window) {
+  $scope.token = $window.localStorage.access_token || '';
+  $scope.signin = function(username, password) {
+    var data = {
+      username: username,
+      password: password
+    };
+    Auth.signin(data)
+      .then(function(user) {
+        if(user === undefined) {
+          alert('no username found!');
+        } else {
+          $window.localStorage.setItem('access_token', user.token);
+          $location.path('/landing');
+        }
+      })
+      .catch(function(err) {
+        //console.log(err);
       });
   };
 });
