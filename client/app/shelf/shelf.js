33a4b13cfabba4c0746f9b02e563b1c4ea06a429
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-01 11:04:10 -0700

    add temp cart window for adding books with total price

--- client/app/shelf/shelf.js
+++ client/app/shelf/shelf.js
@@ -1,12 +1,21 @@
 angular.module('bookstore.shelf', [])
 .controller('ShelfController', function($scope, $routeParams, Shelf, Details, $window) {
   $scope.collections = [];
+  $scope.cart = [];
   $scope.id = $routeParams.id;
+  $scope.totalPrice = 0;
   $scope.getShelf = function() {
     Shelf.getShelf($window.localStorage.getItem('userId'))
       .then(function(collections) {
         console.log(collections);
         $scope.collections = collections;
       });
+  };
+  $scope.addToCart = function(book) {
+    console.log(book);
+    $scope.cart = $scope.cart.concat(book.title);
+    console.log($scope.cart);
+    $scope.totalPrice += Number(book.price);
+    $scope.roundedPrice = Math.floor($scope.totalPrice * 100) / 100;
   };
 })
