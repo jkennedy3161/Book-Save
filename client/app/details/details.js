58270a3a5f861dd9ffa4c825175e83c4ee79ca82
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-01 11:01:59 -0700

    add alert message when deleting a book

--- client/app/details/details.js
+++ client/app/details/details.js
@@ -1,10 +1,17 @@
 angular.module('bookstore.details', [])
-.controller('DetailsController', function($scope, $route, Details) {
+.controller('DetailsController', function($scope, $route, Details, Auth, $location) {
   $scope.book = {};
+  $scope.alert = '';
   $scope.id = $route.current.params.id;
   Details.bookInfo($scope.id)
     .then(function(book) {
       $scope.book = book;
     })
+  $scope.remove = function(bookId) {
+    Auth.removeBook(bookId)
+      .then(function(book) {
+        $scope.alert = book.title + ' removed from your shelf!';
+      })
+  };
 
 });
