692e7d703a58364240e7c2d5c6bc56e213b4153e
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-27 12:15:23 -0700

    updated ec2 server name

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ var config = {
   prod: 'production',
   port: process.env.PORT || 3000,
   //db: {url: 'mongodb://127.0.0.1:27017/books'},
-  db: {url: 'mongodb://jj:test@ec2-34-212-31-149.us-west-2.compute.amazonaws.com:27017/dummyDB'},
+  db: {url: 'mongodb://jj:test@ec2-54-69-209-87.us-west-2.compute.amazonaws.com:27017/dummyDB'},
   expireTime: '10d',
   secrets: {
     jwt: process.env.JWT || 'sushi',
