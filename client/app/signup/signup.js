efadb5d9d5a83c7add2cd9689e2f4521886c92a3
jkennedy3161 <kennedyjt88@gmail.com>
2017-08-03 11:34:25 -0700

    prevent empty users from registering

--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -2,12 +2,14 @@ angular.module('bookstore.signup', [])
 .controller('SignUpController', function($scope, Auth, $window, $location) {
   $scope.signup = function(username, password) {
     var data = {username: username, password: password};
-    Auth.signup(data)
-      .then(function(token) {
-        $window.localStorage.setItem('access_token', token);
-        if($window.localStorage.access_token) {
-          $location.path('/landing');
-        }
-      });
+    if (username && password) {
+      Auth.signup(data)
+        .then(function(token) {
+          $window.localStorage.setItem('access_token', token);
+          if($window.localStorage.access_token) {
+            $location.path('/landing');
+          }
+        });
+    }
   };
 });
