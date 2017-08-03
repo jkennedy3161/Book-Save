341f8f85f19e2ec1d2393d6157e373f59a285dd1
jkennedy3161 <kennedyjt88@gmail.com>
2017-08-03 11:56:42 -0700

    fix issue with registering already used username

--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -2,16 +2,15 @@ angular.module('bookstore.signup', [])
 .controller('SignUpController', function($scope, Auth, $window, $location) {
   $scope.signup = function(username, password) {
     var data = {username: username, password: password};
-    if (username && password) {
-      Auth.signup(data)
-        .then(function(token) {
-          $window.localStorage.setItem('access_token', token);
-          if($window.localStorage.access_token !== undefined) {
-            $location.path('/landing');
-          } else {
-            document.write('user with username already created, use different username');
-          }
-        });
-    }
+    Auth.signup(data)
+      .then(function(token) {
+        if (token !== undefined) $window.localStorage.setItem('access_token', token);
+        console.log($window.localStorage.access_token);
+        if($window.localStorage.access_token) {
+          $location.path('/landing');
+        } else {
+          document.querySelector('#error').innerHTML = 'username ' + username + ' already created, use different username';
+        }
+      });
   };
 });
